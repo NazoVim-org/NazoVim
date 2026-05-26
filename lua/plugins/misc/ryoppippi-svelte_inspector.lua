131fd2545fffed0294d96143efecfb5ad5693cc2
nazozokc <nazozokc@gmail.com>
2026-05-26 23:08:51 +0900

    refactor: cleanup dead code and consolidate duplicate setup() calls (#83)

--- lua/plugins/misc/ryoppippi-svelte_inspector.lua
+++ lua/plugins/misc/ryoppippi-svelte_inspector.lua
@@ -1,6 +1,8 @@
 return {
 	"ryoppippi/vim-svelte-inspector",
 	event = "VeryLazy",
+	-- flatten: open files from browser devtools in current Neovim instance
+	-- fileline: parse file:line:col format from inspector click and jump there
 	dependencies = {
 		"willothy/flatten.nvim",
 		"lewis6991/fileline.nvim",
