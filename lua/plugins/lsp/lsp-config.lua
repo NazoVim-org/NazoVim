569ab004e45b4a0ff6bdb1581ecbd99096a1b1a9
nazozokc <nazozokc@gmail.com>
2026-05-26 23:06:12 +0900

    refactor: cleanup dead code and consolidate duplicate setup() calls

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -32,11 +32,7 @@ return {
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				callback = function(args)
-					local client = vim.lsp.get_client_by_id(args.data.client_id)
 					local bufnr = args.buf
-					if client and client.name == "ts_ls" then
-						client.server_capabilities.semanticTokensProvider = nil
-					end
 					local opts = { buffer = bufnr }
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 					vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, opts)
