75552d53b1901dce041b5d128f78bad69cc0147f
nazozokc <nazozokc@gmail.com>
2026-05-30 21:16:47 +0900

    feat: add avante.nvim AI agent support (#86)
    * feat: add avante.nvim AI agent support
    
    * chore: add avante and Repomap to cspell dictionary

--- lua/plugins/ui/render-markdown-nvim.lua
+++ lua/plugins/ui/render-markdown-nvim.lua
@@ -1,6 +1,6 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	ft = { "markdown" },
+	ft = { "markdown", "Avante" },
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"echasnovski/mini.nvim",
